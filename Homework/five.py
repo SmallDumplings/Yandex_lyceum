1683431e062b8510ab6f62fd082734b513d1edab
NoName <sas.spirin2006@gmail.com>
2023-02-07 18:27:18 +0500

    Edit five.py

--- Homework/five.py
+++ Homework/five.py
@@ -3,7 +3,6 @@ import requests
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit
 from PyQt5.QtGui import QPixmap
-from geocoder import get_coordinate, search
 
 
 class AppWindow(QMainWindow):
@@ -24,28 +23,33 @@ class AppWindow(QMainWindow):
         self.button.resize(50, 30)
 
         self.map_ll = [55.7031122, 37.5308796]
-        self.z = 5
+        self.zoom = 5
         self.map_l = "map"
         self.map_key = "40d1649f-0493-4b70-98ba-98533de7710b"
 
         self.refresh_map()
 
     def refresh_map(self):
-        lon, lat = get_coordinate(self.lineedit.text())
-        ll_spn = f'll={lat},{lon}&spn=0.005,0.005'
-        address = search(f'{lat},{lon}', '0.005,0.005', 'https://static-maps.yandex.ru/1.x/')
-        map_params = {
-            "ll": ",".join(map(str, self.map_ll)),
-            "l": self.map_l,
-            "z": self.z,
-            "ll_spn": ll_spn,
-            "address": address
-        }
-        response = requests.get("https://static-maps.yandex.ru/1.x/", params=map_params)
-        with open("tmp.png", mode="wb") as tmp:
+        geocode_request = 'http://geocode-maps.yandex.ru/1.x/'
+        geocode_params = {'apikey': self.map_key,
+                          'geocode': self.lineedit.text(),
+                          'format': 'json'}
+        geocode_response = requests.get(geocode_request, params=geocode_params)
+        feature = geocode_response.json()['response']['GeoObjectCollection']['featureMember']
+        toponym = feature[0]['GeoObject']
+        coordinates = toponym['Point']['pos']
+        ll = ','.join(coordinates.split())
+
+        map_params = {'l': self.map_l,
+                      'll': ll,
+                      'apikey': self.map_key,
+                      'zoom': self.zoom}
+        request = 'http://static-map.yandex.ru/1.x/'
+        response = requests.get(request, params=map_params)
+        with open('../Image/tmp.png', mode='wb') as tmp:
             tmp.write(response.content)
         pixmap = QPixmap()
-        pixmap.load("tmp.png")
+        pixmap.load('tmp.png')
         self.map.setPixmap(pixmap)
 
 
